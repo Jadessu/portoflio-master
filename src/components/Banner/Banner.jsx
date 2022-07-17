8eb5d06fe46cb90051da2418170b41e82cfd6fcb
Jadessu <junioradessu@gmail.com>
2022-07-17 10:01:50 -0500

    create banner row bottom

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -61,6 +61,40 @@ const banner = {
       </div>
     );
   };
+
+  const BannerRowBottom = ({ title }) => {
+    return (
+      <div className={"banner-row center"}>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ ease: [0.6, 0.01, -0.05, 0.95], duration: 1, delay: 1 }}
+          className='scroll'>
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              ease: "easeInOut",
+              duration: 1,
+              delay: 1.8,
+            }}>
+            scroll
+          </motion.span>
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              ease: "easeInOut",
+              duration: 1,
+              delay: 1.8,
+            }}>
+            down
+          </motion.span>
+        </motion.div>
+        <AnimatedLetters title={title} />
+      </div>
+    );
+  };
   
 
 const Banner = () => {
