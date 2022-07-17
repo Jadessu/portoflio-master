23721ae8ec19aa365755fd845f7c4bd4cabeb4bd
Jadessu <junioradessu@gmail.com>
2022-07-17 10:06:13 -0500

    create banner row center

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -96,6 +96,22 @@ const banner = {
     );
   };
   
+  const BannerRowCenter = ({ title, playMarquee }) => {
+    return (
+      <div className={`banner-row marquee  ${playMarquee && "animate"}`}>
+        <motion.div
+          initial={{ y: 310 }}
+          animate={{ y: 0 }}
+          transition={{ ease: [0.6, 0.01, -0.05, 0.9], duration: 1 }}
+          className='marquee__inner'>
+          <AnimatedLetters title={title} disabled />
+          <AnimatedLetters title={title} />
+          <AnimatedLetters title={title} disabled />
+          <AnimatedLetters title={title} disabled />
+        </motion.div>
+      </div>
+    );
+  };
 
 const Banner = () => {
     const [playMarquee, setPlayMarquee] = useState(false);
