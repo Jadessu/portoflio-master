61f68d073b4720c10b0eb6beb1941154fe83e937
Jadessu <junioradessu@gmail.com>
2022-07-17 10:05:32 -0500

    add styling to banner row bottom

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -64,12 +64,12 @@ const banner = {
 
   const BannerRowBottom = ({ title }) => {
     return (
-      <div className={"banner-row center"}>
+      <div className={`${styles.banner_row} ${styles.center}`}>
         <motion.div
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
           transition={{ ease: [0.6, 0.01, -0.05, 0.95], duration: 1, delay: 1 }}
-          className='scroll'>
+          className={styles.scroll}>
           <motion.span
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
